0d022b87346d5e45cbe8817bd650273043da6af4
Yuelin Wen <wenyuelinca@gmail.com>
2026-03-22 22:04:29 -0400

    fix bug: set rpc timeout to 100ms

--- server/cabinet/primary.go
+++ server/cabinet/primary.go
@@ -99,7 +99,7 @@ func EstablishRPCs(myServerID, numServers, rpcBasePort int) {
 				break
 			}
 			fmt.Printf("[Node %d | Leader    | RPC ] node %d not ready, retrying...\n", myServerID, i)
-			time.Sleep(time.Second)
+			time.Sleep(100 * time.Millisecond)
 		}
 
 		fmt.Printf("[Node %d | Leader    | RPC ] connected to node %d\n", myServerID, i)
